bbe7892c56ae308b3c13c02d8b633c6a3e0e9290
stashuk <st.4528064@gmail.com>
2025-05-06 23:49:31 +0300

    add Registration

--- Liars_deck/MainWindow.xaml.cs
+++ Liars_deck/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using Liars_deck.classes;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -16,15 +17,41 @@ namespace Liars_deck;
 /// </summary>
 public partial class MainWindow : Window
 {
+    private bool isRegMenu = false;
     public MainWindow()
     {
         InitializeComponent();
+        this.KeyDown += MainWindow_KeyDown;
+    }
+    private void ChangeMenu()
+    {
+        if (isRegMenu)
+        {
+            this.GlateLabel.Content = "Мы рады видеть вас снова!";
+            this.WelcomeLabel.Content = "С возвращением!";
+            this.HyperLabel.Content = "Зарегистрироваться";
+            this.Button.Content = "Вход";
+            isRegMenu = !isRegMenu;
+        }
+        else
+        {
+            this.GlateLabel.Content = "Создайте себе новый аккаунт.";
+            this.WelcomeLabel.Content = "Добро пожаловать!";
+            this.HyperLabel.Content = "Войти в аккаунт";
+            this.Button.Content = "Регистрация";
+            isRegMenu = !isRegMenu;
+        }
+    }
+    private void MainWindow_KeyDown(object sender, KeyEventArgs e)
+    {
+        if (e.Key == Key.Escape)
+        {
+            this.Close();
+        }
     }
     private void HyperLabel_MouseDown(object sender, MouseButtonEventArgs e)
     {
-        
-        MessageBox.Show("Сделать переход к регистрации");
-
+        ChangeMenu();
     }
     private void HyperLabel_MouseEnter(object sender, MouseEventArgs e)
     {
@@ -35,9 +62,30 @@ public partial class MainWindow : Window
     {
         HyperLabel.Foreground = Brushes.White;
     }
-
+    private void Exit_MouseDown(object sender, MouseButtonEventArgs e)
+    {
+        this.Close();
+    }
     private void Button_Click(object sender, RoutedEventArgs e)
     {
-        MessageBox.Show("Сделать аутентификацию и регистрацию");
+        User user = new User(this.LoginTextBox.Text, this.PasTextBox.Text);
+        if (!isRegMenu)
+        {
+            
+            if (user.Authorization())
+            {
+
+                MessageBox.Show("Успешная авторизация");
+            }
+        }
+        else
+        {
+            if (user.Registration())
+            {
+                MessageBox.Show("Успешная регистрация");
+                this.PasTextBox.Text = "";
+                ChangeMenu();
+            }
+        }
     }
 }
--- Liars_deck/classes/User.cs
+++ Liars_deck/classes/User.cs
@@ -1,15 +1,131 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace Liars_deck.classes
 {
     class User
     {
-        public int login;
+        public string login;
         private readonly string? password;
-        public int rating;
+        public int? rating;
+        public User(string login, string pas)
+        {
+            this.login = login;
+            this.password = pas;
+            this.rating = 1000;
+        }
+        public bool Authorization()
+        {
+            if (string.IsNullOrEmpty(login) || string.IsNullOrEmpty(password))
+            {
+                MessageBox.Show("Логин и пароль должны быть заполнены");
+                return false;
+            }
+
+            DB db = new DB();
+            DataTable table = new DataTable();
+
+            try
+            {
+                using (MySqlCommand command = new MySqlCommand(
+                    "SELECT `login`, `rating` FROM `users` WHERE `login` = @uL AND `pas` = @uP",
+                    db.getConnection()))
+                {
+                    command.Parameters.Add("@uL", MySqlDbType.VarChar).Value = login;
+                    command.Parameters.Add("@uP", MySqlDbType.VarChar).Value = password;
+
+                    db.openConnection();
+
+                    using (MySqlDataReader reader = command.ExecuteReader())
+                    {
+                        if (reader.HasRows)
+                        {
+                            reader.Read();
+                            this.rating = reader.GetInt32("rating");
+                            return true;
+                        }
+                        else
+                        {
+                            MessageBox.Show("Неверный логин или пароль");
+                            return false;
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка авторизации: {ex.Message}");
+                return false;
+            }
+            finally
+            {
+                db.closeConnection();
+            }
+        }
+
+
+        private bool IsEnterValid()
+        {
+            if (string.IsNullOrEmpty(login) || string.IsNullOrEmpty(password))
+            {
+                MessageBox.Show("Поля должны быть заполнены");
+                return false;
+            }
+            if (login.Contains(' ') || password.Contains(' '))
+            {
+                MessageBox.Show("Поля не должны содержать пробел");
+                return false;
+            }
+            if (password.Length < 8)
+            {
+                MessageBox.Show("Пароль должен иметь хотя бы 8 символов");
+                return false;
+            }
+            DB db = new DB();
+            DataTable table = new DataTable();
+            MySqlDataAdapter adapter = new MySqlDataAdapter();
+
+            MySqlCommand command = new MySqlCommand("SELECT * FROM `users` WHERE `login` = @uL", db.getConnection());
+            command.Parameters.Add("@uL", MySqlDbType.VarChar).Value = login;
+
+            adapter.SelectCommand = command;
+            adapter.Fill(table);
+
+            if (table.Rows.Count > 0)
+            {
+                MessageBox.Show("Пользователь с таким логином уже существует");
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        public bool Registration()
+        {
+            DB db = new DB();
+            MySqlCommand command = new MySqlCommand("INSERT INTO `users` (`login`, `pas`, `rating`) VALUES (@login, @pas, @rating)", db.getConnection());
+            command.Parameters.Add("@login", MySqlDbType.VarChar).Value = login;
+            command.Parameters.Add("@pas", MySqlDbType.VarChar).Value = password;
+            command.Parameters.Add("@rating", MySqlDbType.Int32).Value = 1000;
+
+            db.openConnection();
+
+            if (IsEnterValid())
+            {
+                if (command.ExecuteNonQuery() == 1)
+                    return true;
+                else
+                   return false;
+            }
+            db.closeConnection();
+            return false;
+        }
     }
 }
