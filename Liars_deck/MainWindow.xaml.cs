520dec9f7b6cd1f152025c3d44afed3b725f9bd2
stashuk <st.4528064@gmail.com>
2025-05-06 21:38:48 +0300

    Добавьте файлы проекта.

---
+++ Liars_deck/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Liars_deck;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+    private void HyperLabel_MouseDown(object sender, MouseButtonEventArgs e)
+    {
+        
+        MessageBox.Show("Сделать переход к регистрации");
+
+    }
+    private void HyperLabel_MouseEnter(object sender, MouseEventArgs e)
+    {
+        HyperLabel.Foreground = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FF55A94B"));
+    }
+
+    private void HyperLabel_MouseLeave(object sender, MouseEventArgs e)
+    {
+        HyperLabel.Foreground = Brushes.White;
+    }
+
+    private void Button_Click(object sender, RoutedEventArgs e)
+    {
+        MessageBox.Show("Сделать аутентификацию и регистрацию");
+    }
+}
---
+++ Liars_deck/classes/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Liars_deck.classes
+{
+    class User
+    {
+        public int login;
+        private readonly string? password;
+        public int rating;
+    }
+}
